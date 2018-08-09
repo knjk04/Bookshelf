b2d05e2ff75e3b2c05d54f7df5d73b4e20218d13
Karan Kumar <karan.n.j.kumar@gmail.com>
2018-08-09 21:07:55 +0100

    Sample image now showing using Fresco

--- app/src/main/java/com/presentedbykaran/bookshelf/MainActivity.java
+++ app/src/main/java/com/presentedbykaran/bookshelf/MainActivity.java
@@ -3,26 +3,16 @@ package com.presentedbykaran.bookshelf;
 import android.app.SearchManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.util.Patterns;
 import android.view.Menu;
 import android.widget.ImageView;
 
-//import com.bumptech.glide.Glide;
-//import com.bumptech.glide.request.RequestOptions;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
 
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,16 +22,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_main);
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        imageView = findViewById(R.id.imageView2);
-
-//        Picasso.get().load("http://books.google.com/books/content?id=5QRZ4z6A1WwC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE73eezZyNKE1zd4jSv2IiUUlNwd-fE8KT9Hhaaaup-4Q_nsq2o5747MvQAVC00DQ6d-MAuJ7BRQ_jkhy8fUq19GagSS75c-al97XfLgX9iJyURE-qNLXJ7zh23LmOhAODCMM4sep&source=gbs_api.png").into(imageView);
-        Picasso.get().load("https://i.imgur.com/6zDqjm8.jpg").into(imageView);
+//        Uri uri = Uri.parse("https://raw.githubusercontent.com/facebook/fresco/master/docs/static/logo.png");
+//        Uri uri = Uri.parse("https://i.imgur.com/6zDqjm8.jpg");
+        Uri uri = Uri.parse("https://books.google.com/books/content?id=F1wgqlNi8AMC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api");
+        SimpleDraweeView draweeView = findViewById(R.id.my_image_view);
+        draweeView.setImageURI(uri);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
